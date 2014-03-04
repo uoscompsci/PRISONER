522c873ebe96368ba8592d6c63bf79ba2b1c7af6
ck425 <ck425@st-andrews.ac.uk>
2014-03-04 22:54:26 +0000

    twitter

--- gateway/TwitterGateway.py
+++ gateway/TwitterGateway.py
@@ -44,7 +44,7 @@ class TwitterServiceGateway(ServiceGateway):
 
 		self.resp, self.content = self.client.request(self.request_token_url, "POST", body=self.callback_url)
 		self.timetest = datetime.datetime.now()
-		if self.resp['status'] == '200':
+		if self.resp['status'] != '200':
 		    raise Exception("Invalid response %s." % str(callback))
 
 		self.request_token = dict(urlparse.parse_qsl(self.content))
