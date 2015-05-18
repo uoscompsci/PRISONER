d42ed35b8d2e721744febcc4f4ece8ae12a0404d
Luke Hutton <admin@combustion-studios.com>
2015-05-18 16:27:12 +0100

    policyproc testing

--- workflow/PolicyProcessor.py
+++ workflow/PolicyProcessor.py
@@ -533,7 +533,7 @@ class PolicyProcessor(object):
 			to_match_obj =	self._infer_attributes(to_match,
 			response.content)
 
-			print "does %s equal %s" % to_match_obj, on_objec_obj)
+			print "does %s equal %s" % (to_match_obj, on_object_obj)
 			if to_match_obj == on_object_obj:
 				return True
 			else:
