cd978abc31fba1e3aa8f23660937732c93cb3a47
Luke Hutton <luke@lukehutton.co.uk>
2015-09-09 13:21:52 +0100

    fix package paths in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='PRISONER',
       author='Luke Hutton',
       author_email='lh49@st-andrews.ac.uk',
       license='BSD',
-      packages=['gateway','persistence','server','tests','tools','workflow'],
+      packages=['prisoner'],
       zip_safe=False,
       install_requires=[
           ],
